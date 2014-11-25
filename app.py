57002dfa5658fe2ad0ccbee39520b6b98d7a9f40
Matthew Adams <im.matthew.adams@gmail.com>
2014-11-25 16:08:34 -0600

    routes with parameters

--- app.py
+++ app.py
@@ -6,5 +6,10 @@ app = Flask(__name__)
 def index():
     return 'Index'
 
+
+@app.route('/posts/<post_id>')
+def show_post(post_id):
+    return 'Post ' + post_id
+
 if __name__ == '__main__':
     app.run(debug=True)
