ed80210da86d442ddfe4d29d41ee24b5bc286bba
Matthew Adams <im.matthew.adams@gmail.com>
2014-11-28 14:38:00 -0600

    setting up requires

--- liberchan/views.py
+++ liberchan/views.py
@@ -1,6 +1,10 @@
 from liberchan import app
 
 
+@app.route('/', methods=['GET'])
+def index():
+    return app.send_static_file('index.html')
+
 @app.route('/<post_id>/', methods=['GET'])
 def show_post(post_id):
     return 'Post ' + post_id
