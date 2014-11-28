4ccdc7ec3f9e79e5e530bf79e475e4718438a9bd
Matthew Adams <im.matthew.adams@gmail.com>
2014-11-28 14:03:45 -0600

    serve static files from root url

--- liberchan/views.py
+++ liberchan/views.py
@@ -1,11 +1,6 @@
 from liberchan import app
 
 
-@app.route('/', methods=['GET'])
-def index():
-    return app.send_static_file('index.html')
-
-
 @app.route('/<post_id>/', methods=['GET'])
 def show_post(post_id):
     return 'Post ' + post_id
